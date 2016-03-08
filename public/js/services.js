ce7c2b9bdbf63715e4d58674fb41403a524cf5d8
Jiapei Liang <liang98@purdue.edu>
2016-03-08 10:49:12 -0500

    Set up service

--- public/js/services.js
+++ public/js/services.js
@@ -5,5 +5,34 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+
+.factory('pagesFactory', ['$http', function ($http) {
+	return {
+		getPages: function () {
+			return $http.get('/api/pages');
+		},
+
+		savePages: function (pageData) {
+			var id = pageData._id;
+
+			if (id === 0) {
+				return $http.post('/api/pages/add', pageData);
+			} else {
+				return $http.post)'/api/pages/update', pageData);
+			}
+		},
+
+		deletePage: function (id) {
+			return $http.get('/api/pages/delete/' + id);
+		},
+
+		getPageContentById: function (id) {
+			return $http.get('/api/pages/page/' + id);
+		},
+
+		getPageContentByUrl: function (url) {
+			return $http.get('/api/pages/details/' + url);
+		}
+	};
+}]);
