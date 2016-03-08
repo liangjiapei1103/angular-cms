da10c20d27901c7a407c69679b7a1ca6962bb163
Jiapei Liang <liang98@purdue.edu>
2016-03-08 10:59:23 -0500

    Add bootstrap via bower

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  .controller('AdminPagesCtrl', ['$scope', '$log', 'pagesFactory', function($scope, $log, pagesFactory) {
+  	pagesFactory.getPages().then(
+  		function (res) {
+  			$scope.allPages = res.data;
+  		}, 
+  		function (err) {
+  			$log.error(err);
+  	});
+
+  	$scope.deletePage = function (id) {
+  		pagesFactory.deletePage(id);
+  	};
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+}]);
